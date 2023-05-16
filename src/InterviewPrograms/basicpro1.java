b6d6417bed2d298201879e0dcf09a0d7dfa7ec1e
accoun2000 <patilsanjana761@gmail.com>
2023-05-16 16:31:27 +0530

    javaclassprograms

---
+++ src/InterviewPrograms/basicpro1.java
@@ -0,0 +1,11 @@
+//Write a program to print 'Hello' on screen and then print your name on a separate line.
+
+
+package InterviewPrograms;
+
+public class basicpro1 {
+        public static void main(String[] args) {
+                System.out.println("Hello World");
+                System.out.println("I am Sanjana Patil");
+        }
+}
